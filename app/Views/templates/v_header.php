e1f7514a0f51c61ff7fbebd95087b685db6eff9f
rilofebrian <41282684+rilofebrian@users.noreply.github.com>
2021-09-22 13:09:47 +0700

    Update v_header.php

--- app/Views/templates/v_header.php
+++ app/Views/templates/v_header.php
@@ -13,7 +13,7 @@
     <title> <?= $judul;?> </title>
 
     <!-- Custom fonts for this template-->
-    <script src="https://use.fontawesome.com/be7e78cad7.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" >
     <!-- Custom styles for this template-->
